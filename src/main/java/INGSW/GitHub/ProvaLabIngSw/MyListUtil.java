9785ed46872dcdbbe13772f2e3ad4ff1e9a70abe
Vadim Muto <mutovadim@gmail.com>
2020-11-04 11:13:03 +0100

    Initial project

---
+++ src/main/java/INGSW/GitHub/ProvaLabIngSw/MyListUtil.java
@@ -0,0 +1,14 @@
+package INGSW.GitHub.ProvaLabIngSw;
+
+import java.util.List;
+
+public class MyListUtil {
+	
+	public void ordinaCrescente(List<Integer> numbers) {
+		
+	}
+	
+	public void ordinaDecrescente(List<Integer> numbers) {
+		
+	}
+}
