c90770823c2b8e2f424dde7ba038dc76b11c8169
Vadim Muto <mutovadim@gmail.com>
2020-11-04 11:20:47 +0100

    Modify MyTestUtil

--- src/main/java/INGSW/GitHub/ProvaLabIngSw/MyListUtil.java
+++ src/main/java/INGSW/GitHub/ProvaLabIngSw/MyListUtil.java
@@ -1,14 +1,26 @@
 package INGSW.GitHub.ProvaLabIngSw;
 
-import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 public class MyListUtil {
 	
-	public void ordinaCrescente(List<Integer> numbers) {
-		
+	public void ordinaCrescente(ArrayList<Integer> numbers) {
+		Collections.sort(numbers, new Comparator<Integer>() {
+			@Override
+			public int compare(Integer o1, Integer o2) {
+				return o1.compareTo(o2);
+			}
+		});
 	}
 	
-	public void ordinaDecrescente(List<Integer> numbers) {
-		
+	public void ordinaDecrescente(ArrayList<Integer> numbers) {
+		Collections.sort(numbers, new Comparator<Integer>() {
+			@Override
+			public int compare(Integer o1, Integer o2) {
+				return o1.compareTo(o2) * -1;
+			}
+		});
 	}
 }
